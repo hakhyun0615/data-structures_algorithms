edca2b355a9c55e395fcfdf83385440544b8ad9c
KimHakHyun <hakhyun0615@naver.com>
2024-12-04 10:45:17 +0900

    main

--- data structures/utils.py
+++ data structures/utils.py
@@ -35,3 +35,27 @@ class Node:
     
     def set_next(self, new_next):
         self.next = new_next
+
+class BTNode:
+    def __init__(self, item, left_child = None, right_child = None):
+        self.key = item
+        self.left = left_child
+        self.right = right_child
+
+    def get_key(self): 
+        return self.key
+    
+    def get_left(self): 
+        return self.left
+    
+    def get_right(self):
+        return self.right
+    
+    def set_key(self, new_item):
+        self.key = new_item
+    
+    def set_left(self, new_left_child):
+        self.left = new_left_child
+    
+    def set_right(self, new_right_child):
+        self.right = new_right_child
